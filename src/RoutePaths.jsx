22df0afdacc4bae4dca96ca11eef672915b1ec29
Céline Gourc <celine.gourc1@laposte.net>
2023-04-24 11:59:09 +0200

    error 404

--- src/RoutePaths.jsx
+++ src/RoutePaths.jsx
@@ -2,10 +2,15 @@ import {createBrowserRouter} from "react-router-dom";
 import Home from './pages/Home/Home'
 import Form from './pages/Form/Form'
 import Recipe from './pages/Recipe/Recipe'
+import Error404 from './pages/404/Error404'
 
 const router = createBrowserRouter([
     {
       path: "/",
+      element: <Home/>,
+    },
+    {
+      path: "/MealPlanner",
       element: <Home/>,
     },
     {
@@ -16,7 +21,12 @@ const router = createBrowserRouter([
         path: "/idee-repas",
         element: <Recipe />,
       },
+      {
+        path: "/*",
+        element: <Error404 />,
+      },
   ]);
 
   export default router;
+
 
---
+++ src/pages/404/Error404.jsx
@@ -0,0 +1,12 @@
+import {Link} from 'react-router-dom'
+
+
+function Error404(){
+    return(
+        <div>
+            <Link to ='/'>retour à l'accueil</Link>
+        </div>
+    )
+}
+
+export default Error404
