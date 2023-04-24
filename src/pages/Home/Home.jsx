4c5b6961f7b5a55d9dd05d5afda6cb1c7389ce4a
Céline Gourc <celine.gourc1@laposte.net>
2023-04-24 10:10:45 +0200

    balises meta

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,7 +8,8 @@ function Home(){
     return(
         <div className ={styles.home_container}>
         <Banner />
-        <p>Simplifiez-vous la vie ! Imprimez vos menus pour la semaine, préparez votre liste de courses et n'y pensez plus.</p>
+        <p>Je me simplifie la vie ! 
+            J'imprime mes menus pour la semaine, je prépare ma liste de courses et je n'y pense plus.</p>
         <div className ={styles.button_container}>
         <Link to='/form' className = {styles.button_home}> Je renseigne et imprime mes menus de la semaine</Link>
         <Link to = "/idee-repas"className ={styles.button_home}> Je n'ai pas d'idées de repas</Link>
